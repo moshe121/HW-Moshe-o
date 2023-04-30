4c56fb25a7737906031b49a29b5ddd8752935909
moshe <mosheoh8@gmail.com>
2023-05-01 00:35:00 +0300

    תרגיל מס הכנסה

---
+++ תרגיל מס הכנסה/js/main.js
@@ -0,0 +1,18 @@
+var salary = prompt("אנא הזן את משכורתך");
+var tax = 0;
+if (salary < 25000) {
+    tax = salary / 10;
+}
+if (salary > 25000 && salary < 50000) {
+    tax = 2500 + (salary - 25000) / 5;
+}
+if (salary > 50000 && salary < 100000) {
+    tax = 7500 + ((salary - 50000) / 10) * 3;
+}
+if (salary > 100000 && salary < 150000) {
+    tax = 22500 + ((salary - 100000) / 10) * 4;
+}
+if (salary > 150000) {
+    tax = 42500 + ((salary - 150000) / 10) * 5;
+}
+alert(tax);
